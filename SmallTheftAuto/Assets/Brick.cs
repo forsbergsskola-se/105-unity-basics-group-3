20a88bb01920c8d39b8774c60f5958188420ac59
M1crob1ology <lucabelluso123@gmail.com>
2022-10-25 14:06:30 +0200

    Completed Event Functions exercise 3

--- SmallTheftAuto/Assets/Brick.cs
+++ SmallTheftAuto/Assets/Brick.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Brick : MonoBehaviour
+public class Brick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public float minRandomOffset = -0.05f;
     public float maxRandomOffset = 0.05f;
@@ -10,5 +11,25 @@ public class Brick : MonoBehaviour
     void Start()
     {
         transform.position += transform.right * Random.Range(minRandomOffset, maxRandomOffset);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Plane plane = new Plane(Vector3.up, Vector3.up * eventData.pointerPressRaycast.worldPosition.y);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            transform.position = ray.GetPoint(distance);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
     }
 }
