f16920dd845d358b39e3be2a9ff2d1ad0356c822
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-26 21:42:29 +0200

    Offset bricks

--- SmallTheftAuto/Assets/Scripts/Brick.cs
+++ SmallTheftAuto/Assets/Scripts/Brick.cs
@@ -7,7 +7,7 @@ public class Brick : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = transform.position + (transform.right * Random.Range(-0.1f, 0.1f));
     }
 
     // Update is called once per frame
