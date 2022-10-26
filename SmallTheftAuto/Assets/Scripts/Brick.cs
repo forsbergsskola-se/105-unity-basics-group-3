44a8d7ec6cca604339e94671a7a7fa4f75fbd752
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-26 08:58:00 +0200

    add brick script
    can't get transform to work

--- SmallTheftAuto/Assets/Scripts/Brick.cs
+++ SmallTheftAuto/Assets/Scripts/Brick.cs
@@ -7,8 +7,7 @@ public class Brick : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Transform transform {get;}
-        Transform Vector3 position {get;set;}
+        
     }
 
     // Update is called once per frame
