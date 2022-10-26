61d9de40413ccc9d3cca34d9b8221171ef8cd605
KaleSmug <karl.sandegren@gmail.com>
2022-10-26 10:32:06 +0200

    Drag and drop

--- SmallTheftAuto/Assets/Scripts/Brick.cs
+++ SmallTheftAuto/Assets/Scripts/Brick.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Brick : MonoBehaviour
+public class Brick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
     private Material material;
@@ -16,5 +17,25 @@ public class Brick : MonoBehaviour
     void Update()
     {
         
+    } 
+    public void OnDrag(PointerEventData eventData)
+    {
+        Plane plane = new Plane(Vector3.up, Vector3.up * eventData.pointerPressRaycast.worldPosition.y);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            transform.position = ray.GetPoint(distance);
+        }
     }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
 }
