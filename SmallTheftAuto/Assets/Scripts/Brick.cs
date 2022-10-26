7125b2c6edf47df6276a8f1b16ba13b20d0f35d1
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-26 02:35:36 +0200

    Add Dead Zone script

---
+++ SmallTheftAuto/Assets/Scripts/Brick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform transform {get;}
+        Transform Vector3 position {get;set;}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- SmallTheftAuto/Assets/Scripts/DeadZone.cs
+++ SmallTheftAuto/Assets/Scripts/DeadZone.cs
@@ -1,8 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeadZone : MonoBehaviour
 {
-    
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("The tower collapsed. You lose.");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
