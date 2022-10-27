c652ae9ee536fda00db04726a5704aa10b4d2299
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-27 22:05:56 +0200

    use get.axis for movement

--- SmallTheftAuto/Assets/Scripts/PlayerMovement.cs
+++ SmallTheftAuto/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnAngle = 150;
+    public float turnSpeed = 150;
 
     public float verticalPower = 20;
     // Start is called before the first frame update
@@ -17,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
 void Update()
 {
     // only, if the W-Key is currently pressed...
-    if (Input.GetKey(KeyCode.W))
+    /*if (Input.GetKey(KeyCode.W))
     {
         // translate the player's transform-component on the y-axis (which points up)
         transform.Translate(0f, verticalPower * Time.deltaTime, 0f);
@@ -29,11 +29,16 @@ void Update()
     }
     if (Input.GetKey(KeyCode.A))
     {
-        transform.Rotate(0f, -turnAngle * Time.deltaTime, 0f);
+        transform.Rotate(0f, -turnSpeed * Time.deltaTime, 0f);
     }
     if (Input.GetKey(KeyCode.D))
     {
-        transform.Rotate(0f, turnAngle * Time.deltaTime, 0f);
-    }
+        transform.Rotate(0f, turnSpeed * Time.deltaTime, 0f);
+    }*/
+    float translation = Input.GetAxis("Vertical") * verticalPower * Time.deltaTime;
+    transform.Translate(0, 0, translation);
+    
+    float rotation = Input.GetAxis("Horizontal") * turnSpeed  * Time.deltaTime;
+    transform.Rotate(0, rotation, 0);
 }
 }
