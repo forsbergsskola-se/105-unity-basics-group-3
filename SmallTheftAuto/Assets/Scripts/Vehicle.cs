56a804b4be181996db1469e75e46645db12f1068
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-01 09:19:31 +0100

    create parking spots

--- SmallTheftAuto/Assets/Scripts/Vehicle.cs
+++ SmallTheftAuto/Assets/Scripts/Vehicle.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Object = UnityEngine.Object;
@@ -16,54 +17,55 @@ public class Vehicle : MonoBehaviour
 
     }
 
-    void Awake()
-    {
-        //player.SetActive(true);
-    }
-    
     void Update()
     {
         EnterCarButtonPressed();
     }
 
-    bool EnterCarButtonPressed()
+    void EnterCarButtonPressed()
     {
-        
+
         if (Vector3.Distance(car.transform.position, player.transform.position) < 2)
         {
             if (Input.GetKey(KeyCode.F))
             {
-            if (PlayerIsInCar())
-                // Already in Car, so get out of car
-                LeaveCar();
-            else
-                // Not in Car, get in
-                EnterCar();
+                if (Vector3.Distance(car.transform.position, player.transform.position) < 2)
+                {
+                    if (PlayerIsInCar())
+                        // Already in Car, so get out of car
+                        LeaveCar();
+                    else
+                        // Not in Car, get in
+                        EnterCar();
+                }
             }
         }
-        return true;
+
+        bool PlayerIsInCar()
+        {
+            // active in hierarchy
+            if (player.activeInHierarchy)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        void EnterCar()
+        {
+            Console.WriteLine("Get in Car");
+            player.SetActive(false);
+            carMovement.enabled = true;
+        }
+
+        void LeaveCar()
+        {
+            Console.WriteLine("Get out of Car");
+            player.SetActive(true);
+            carMovement.enabled = false;
+        }
+    }
 }
 
-    bool PlayerIsInCar()
-    {
-        // active in hierarchy
-        if (player.activeInHierarchy)
-        {
-            return false;
-        }
-        return true;
-    }
-    
-    void EnterCar()
-    {
-        Console.WriteLine("Get in Car");
-        player.SetActive(false);
-        carMovement.enabled = true;
-    }
-    void LeaveCar()
-    {
-        Console.WriteLine("Get out of Car");
-        player.SetActive(true);
-        carMovement.enabled = false;
-    }
-}
+
