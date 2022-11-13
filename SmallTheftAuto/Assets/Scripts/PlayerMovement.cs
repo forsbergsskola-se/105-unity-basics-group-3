0cdc65f22fa20c7f4a40fc5f475f59374a450202
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-13 03:19:02 +0100

    Reset Input System

--- SmallTheftAuto/Assets/Scripts/PlayerMovement.cs
+++ SmallTheftAuto/Assets/Scripts/PlayerMovement.cs
@@ -1,88 +1,22 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public PlayerControls controls;
-    
-    private Vector2 inputVector = new Vector2(0, 0);
-
-    private Rigidbody rb;
-    //private PlayerControls _playerControls;
 
     [SerializeField] private float turnSpeed = 150;
 
     [SerializeField] private float speed = 200;
 
-    private void Awake()
+    void Update()
     {
-        controls.PlayerActions.Shoot.performed += ctx => Shoot();
-    }
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void Shoot()
-    {
-        Debug.Log("We shot the sheriff");
-    }
-    public void Moving(InputAction.CallbackContext context)
-    {
-        inputVector = context.ReadValue<Vector2>();
-        Debug.Log("X: " + inputVector.x.ToString() + " Z: " + inputVector.y.ToString());
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = transform.forward * (inputVector.y * speed * Time.deltaTime);
-    }
-
-    private void OnEnable()
-    {
-        controls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        controls.Disable();
-    }
-
-    /*private void Awake()
-    {
-        _playerControls = new PlayerControls();
-    }
-
-    private void OnEnable()
-    {
-        _playerControls.Enable();
-        //_playerControls.PlayerActions.WASD.performed += WASD;
-    }
-
-    private void OnDisable()
-    {
-        _playerControls.Disable();
-        //_playerControls.PlayerActions.WASD.performed -= WASD;
-    }
-
-    private void FixedUpdate()
-    {
-        float movement = _playerControls.PlayerActions.WASD.ReadValue<float>();
-        Vector3 currentPosition = transform.position;
-        currentPosition.x = movement * verticalPower * Time.deltaTime;
-        currentPosition.z = movement * verticalPower * Time.deltaTime;
-        transform.position = currentPosition;
-    }*/
-
-    /*void Update()
-    {
-        float translation = Input.GetAxis("Vertical") * verticalPower * Time.deltaTime;
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(0,0 , translation);
     
         float rotation = Input.GetAxis("Horizontal") * turnSpeed  * Time.deltaTime;
         transform.Rotate(0, rotation, 0);
-    }*/
+    }
 }
