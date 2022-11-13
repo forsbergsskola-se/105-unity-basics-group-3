e7982b20a8ddd3cc0f82eb85235494b8e8c265aa
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-13 04:41:45 +0100

    Create script to handle F key

---
+++ SmallTheftAuto/Assets/Scripts/FKeyPress.cs
@@ -0,0 +1,17 @@
+//Handle F key to enter / exit car and other interactions
+using UnityEngine;
+
+public class FKeyPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
